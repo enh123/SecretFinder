cea544e3ce8f77eb779094935de0cde52ea65cd3
enh <134757026+enh123@users.noreply.github.com>
2025-04-20 13:16:07 +0800

    Update search.py

--- core/search.py
+++ core/search.py
@@ -1,52 +1,39 @@
 import os
-import re
 import signal
 import sys
-from concurrent.futures import ThreadPoolExecutor
+from concurrent.futures.thread import ThreadPoolExecutor
+from multiprocessing import Lock
 
-import pyfiglet
 import requests
 from colorama import Fore, init
 from tqdm import tqdm
 
-from modules import config, find_param, find_path, find_subdomain
+from modules import config
+from modules import find_subdomain, find_param, find_path
 
-init(autoreset=True, strip=False)  # 添加 strip=False
+init(autoreset=True, strip=False)  # strip=False 表示不移除 ANSI 转义序列。
 requests.packages.urllib3.disable_warnings()
+
 
 class Search:
     def __init__(self):
-        self.url = config.get_value("url")
-        self.url_list = config.get_value("url_list")
-        self.domain = config.get_value("domain")
-        self.threads = config.get_value("threads")
-        self.proxy = config.get_value("proxy")
-        self.output_file_name = config.get_value("output_file_name")
-        self.headers = {
-            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:128.0) Gecko/20100101 Firefox/128.0"
-        }
-        try:
-            headers = config.get_value("headers")
-            for header in headers:
-                parts = header.split(':')
-                key = parts[0].strip()
-                value = parts[1].strip()
-                if key and value:
-                    self.headers[key] = value
-        except Exception as e:
-            pass
-
-        self.session = requests.session()
+        self.url = config.get_args("url")
+        self.url_list = config.get_url_list()
+        self.proxy = config.get_args("proxy")
+        self.threads = config.get_args("threads")
+        self.timeout = config.get_args("timeout")
         self.pattern_description = config.get_pattern_description()
+        self.headers = config.get_args("headers")
+        self.domain_list = config.get_args("domain_list")
+        self.domain = config.get_args("domain")
         self.data_dict = {pattern: {} for pattern, description in self.pattern_description}
-
         self._stop_flag = False
         # 注册信号处理
         signal.signal(signal.SIGINT, self._signal_handler)
+        self.lock = Lock()
+        self.session = requests.session()
 
-        self.subdomain_list = []
-
-    def _signal_handler(self,):
+    def _signal_handler(self, signum, frame):
         """处理终止信号"""
         self._stop_flag = True
         print("\n" + Fore.RED + "正在强制终止...")
@@ -56,65 +43,53 @@ class Search:
         if self._stop_flag:
             return  # 提前终止检查
         try:
-            response = self.session.get(url.strip(), headers=self.headers, proxies=self.proxy, verify=False, timeout=30)
-
+            response = self.session.get(url.strip(), headers=self.headers, proxies=self.proxy, verify=False,
+                                        timeout=self.timeout)
         except Exception as e:
-            if "-f" in sys.argv and "-u" not in sys.argv:
-                process_bar.update(1)
-            return
-        if self._stop_flag:
+            if self.url_list and not self.url:
+                with self.lock:
+                    process_bar.update(1)
             return
 
-        if response.text and response.status_code!=404:
+        if response.text and response.status_code != 404:
             for pattern, description in self.pattern_description:
-                matched_data = pattern.findall(response.text)
-                if matched_data:
-                    self.data_dict[pattern][url.strip()] = matched_data
+                try:
+                    data = pattern.findall(response.text)
+                    if data:
+                        self.data_dict[pattern][url.strip()] = data
+                except:
+                    pass
 
-            find_path.find_path(response.text)
-            find_subdomain.find_subdomain(response.text)
+            find_subdomain.find(response.text)
+            find_path.find(response.text)
 
-            if self.url and not self.url_list:
-
-                if self.domain:
-                    if ',' in self.domain:
-                        domains = self.domain.split(',')
-
-            if self.domain is not None and ('-d' in sys.argv or '--domain' in sys.argv):
-                if ',' in self.domain:
-                    domains = self.domain.split(',')
-                    for domain in domains:
-                        regex = r'(?:[a-zA-Z0-9_-]+\.)+' + domain
-                        subdomains = re.findall(regex, response.text, re.IGNORECASE)
-                        if subdomains:
-                            for subdomain in subdomains:
-                                self.subdomain_list.append(subdomain.strip())
-
-        if "-f" in sys.argv and "-u" not in sys.argv:
-            process_bar.update(1)
+        with self.lock:
+            if self.url_list and not self.url:
+                process_bar.update(1)
 
     def print_result(self):
         is_None = True
+        # data_dict={
+        # pattern:{ url:[data,data],url:[data,data] },
+        # pattern:{ url:[data,data],url:[data,data] }, ....
+        # }
         for pattern, description in self.pattern_description:
-            urls_data = self.data_dict[pattern]
-            if urls_data != {}:  # 不为空
-                is_None = False
-
-        if is_None and not (config.get_subdomain_list() and config.get_path_list()):
+            for url_data in self.data_dict[pattern]:
+                if url_data != {}:
+                    is_None = False
+        if is_None and config.get_subdomain_list() and config.get_path_list:
             sys.exit("\n" + Fore.RED + "未找到敏感信息")
 
         for pattern, description in self.pattern_description:
-            urls_data = self.data_dict[pattern]
+            urls_data = self.data_dict[pattern]  # urls_data=[url:[data,data],url:[data,data]]
             if urls_data:
                 print(f"\n{Fore.YELLOW}{description}:")
 
-            for url, matched_data in urls_data.items():
+            for url, data in urls_data.items():
                 print(Fore.CYAN + "result from: " + url + ":")
-
-                for data in set(matched_data):
-                    print(f"{Fore.RED}{data}")
+                for data_item in set(data):
+                    print(f"{Fore.RED}{data_item}")
                 print()  # 输出换行
-
         if config.get_subdomain_list():
             print('\n' + Fore.YELLOW + "子域名:")
             for subdomain in config.get_subdomain_list():
@@ -125,9 +100,9 @@ class Search:
             for path in config.get_path_list():
                 print(f"{Fore.RED}{path}")
 
-    def muiltiple_thread(self):
+    def multi_thread(self):
         with tqdm(total=len(self.url_list), desc="process",
-                  bar_format="{l_bar}{bar} {n_fmt}/{total_fmt} time [{elapsed}]") as process_bar:
+                  bar_format="{l_bar}{bar}{n_fmt}/{total_fmt} time: [{elapsed}]") as process_bar:
             with ThreadPoolExecutor(max_workers=self.threads, ) as executor:
                 future_list = []
                 for url in self.url_list:
@@ -144,8 +119,7 @@ def main():
         sys.exit()
 
     if search.url and not search.url_list:
-        search.match_data(config.get_value("url"))
+        search.match_data(search.url)
     if not search.url and search.url_list:
-        search.muiltiple_thread()
-
+        search.multi_thread()
     search.print_result()

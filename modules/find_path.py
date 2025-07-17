babb01287880dc1c759fe98c3d69d97fc03db1da
enh <134757026+enh123@users.noreply.github.com>
2025-07-17 17:47:12 +0800

    Update find_path.py

--- modules/find_path.py
+++ modules/find_path.py
@@ -3,7 +3,49 @@ import re
 from modules import config
 
 # hae 正则
-pattern = r'''(?:"|')(((?:[a-zA-Z]{1,10}://|//)[^"'/]{1,}\.[a-zA-Z]{2,}[^"']{0,})|((?:/|\.\./|\./)[^"'><,;|*()(%%$^/\\\[\]][^"'><,;|()]{1,})|([a-zA-Z0-9_\-/]{1,}/[a-zA-Z0-9_\-/]{1,}\.(?:[a-zA-Z]{1,4}|action)(?:[\?|#][^"|']{0,}|))|([a-zA-Z0-9_\-/]{1,}/[a-zA-Z0-9_\-/]{3,}(?:[\?|#][^"|']{0,}|))|([a-zA-Z0-9_\-]{1,}\.(?:\w)(?:[\?|#][^"|']{0,}|)))(?:"|')'''
+# pattern = r'''(?:"|')(((?:[a-zA-Z]{1,10}://|//)[^"'/]{1,}\.[a-zA-Z]{2,}[^"']{0,})|((?:/|\.\./|\./)[^"'><,;|*()(%%$^/\\\[\]][^"'><,;|()]{1,})|([a-zA-Z0-9_\-/]{1,}/[a-zA-Z0-9_\-/]{1,}\.(?:[a-zA-Z]{1,4}|action)(?:[\?|#][^"|']{0,}|))|([a-zA-Z0-9_\-/]{1,}/[a-zA-Z0-9_\-/]{3,}(?:[\?|#][^"|']{0,}|))|([a-zA-Z0-9_\-]{1,}\.(?:\w)(?:[\?|#][^"|']{0,}|)))(?:"|')'''
+
+#linkfinder 正则
+pattern = r"""
+
+  (?:"|')                               # Start newline delimiter
+
+  (
+    ((?:[a-zA-Z]{1,10}://|//)           # Match a scheme [a-Z]*1-10 or //
+    [^"'/]{1,}\.                        # Match a domainname (any character + dot)
+    [a-zA-Z]{2,}[^"']{0,})              # The domainextension and/or path
+
+    |
+
+    ((?:/|\.\./|\./|\#)                    # Start with /,../,./
+    [^"'><,;| *()(%%$^\\\[\]]          # Next character can't be...
+    [^"'><,;|()]{1,})                   # Rest of the characters can't be
+
+    |
+
+    ([a-zA-Z0-9_\-/]{1,}/               # Relative endpoint with /
+    [a-zA-Z0-9_\-/.]{1,}                # Resource name
+    \.(?:[a-zA-Z]{1,4}|action)          # Rest + extension (length 1-4 or action)
+    (?:[\?|#][^"|']{0,}|))              # ? or # mark with parameters
+
+    |
+
+    ([a-zA-Z0-9_\-/]{1,}/               # REST API (no extension) with /
+    [a-zA-Z0-9_\-/]{3,}                 # Proper REST endpoints usually have 3+ chars
+    (?:[\?|#][^"|']{0,}|))              # ? or # mark with parameters
+
+    |
+
+    ([a-zA-Z0-9_\-]{1,}                 # filename
+    \.(?:php|asp|aspx|jsp|json|
+         action|html|js|txt|xml)        # . + extension
+    (?:[\?|#][^"|']{0,}|))              # ? or # mark with parameters
+
+  )
+
+  (?:"|')                               # End newline delimiter
+
+"""
 
 pattern = re.compile(pattern, re.VERBOSE)
 
@@ -15,11 +57,12 @@ def find(response_text):
         '.css', '.jpg', '.png', '.woff', '.swf'
                                          '.bmp', '.gif', '.ico', '.jpeg', '.tiff', '.webp',
         '.eot', '.otf', '.ttf', '.woff2',
-        '.mp3', '.mp4', '.avi', '.mov', '.wav',
+        '.mp3', '.mp4', '.avi', '.mov', '.wav', '.vue'
     ]
 
     for match in matches:
         path = match[0]
+
         if ("http://" not in path and "https://" not in path) and not any(
                 i in match[0].lower() for i in black_list):
             # if "./" in path:
@@ -28,8 +71,9 @@ def find(response_text):
                 if path:
                     if path.startswith('/'):
                         path = path[1:]
+                    elif path.startswith("#"):
+                        path = path[1:]
                     elif path.startswith('.'):
                         path = path[1:]
-
             config.set_path(path.strip())
     return

f28d3da5873d2c69b2de8b31fb4451d62a25d428
enh <134757026+enh123@users.noreply.github.com>
2025-07-20 10:18:35 +0800

    Update find_path.py

--- modules/find_path.py
+++ modules/find_path.py
@@ -5,7 +5,7 @@ from modules import config
 # hae 正则
 # pattern = r'''(?:"|')(((?:[a-zA-Z]{1,10}://|//)[^"'/]{1,}\.[a-zA-Z]{2,}[^"']{0,})|((?:/|\.\./|\./)[^"'><,;|*()(%%$^/\\\[\]][^"'><,;|()]{1,})|([a-zA-Z0-9_\-/]{1,}/[a-zA-Z0-9_\-/]{1,}\.(?:[a-zA-Z]{1,4}|action)(?:[\?|#][^"|']{0,}|))|([a-zA-Z0-9_\-/]{1,}/[a-zA-Z0-9_\-/]{3,}(?:[\?|#][^"|']{0,}|))|([a-zA-Z0-9_\-]{1,}\.(?:\w)(?:[\?|#][^"|']{0,}|)))(?:"|')'''
 
-#linkfinder 正则
+#经过修改的 linkfinder 正则
 pattern = r"""
 
   (?:"|')                               # Start newline delimiter
@@ -17,7 +17,7 @@ pattern = r"""
 
     |
 
-    ((?:/|\.\./|\./|\#)                    # Start with /,../,./
+    ((?:/|\.\./|\./|\#)                    # Start with /,../,./,#
     [^"'><,;| *()(%%$^\\\[\]]          # Next character can't be...
     [^"'><,;|()]{1,})                   # Rest of the characters can't be
 
@@ -56,23 +56,23 @@ def find(response_text):
     black_list = [
         '.css', '.jpg', '.png', '.woff', '.swf'
                                          '.bmp', '.gif', '.ico', '.jpeg', '.tiff', '.webp',
-        '.eot', '.otf', '.ttf', '.woff2',
-        '.mp3', '.mp4', '.avi', '.mov', '.wav', '.vue'
+        '.eot', '.otf', '.ttf', 'woff2',
+        '.mp3', '.mp4', '.avi', '.mov', '.wav', '.vue', 'text/javascript', 'text/css','application/json','text/json'
     ]
 
     for match in matches:
         path = match[0]
-
         if ("http://" not in path and "https://" not in path) and not any(
                 i in match[0].lower() for i in black_list):
+
             # if "./" in path:
             #    path = path.replace("./", '\n'+'/')
             for i in range(1, 6):
                 if path:
                     if path.startswith('/'):
                         path = path[1:]
-                    elif path.startswith("#"):
-                        path = path[1:]
+                    # elif path.startswith("#"):
+                    #    path = path[1:]
                     elif path.startswith('.'):
                         path = path[1:]
             config.set_path(path.strip())

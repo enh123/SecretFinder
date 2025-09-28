67b72c719f03fc407c1315baac1b271de84f00f9
enh <134757026+enh123@users.noreply.github.com>
2025-09-28 16:50:06 +0800

    Update find_path.py

--- modules/find_path.py
+++ modules/find_path.py
@@ -5,7 +5,7 @@ from modules import config
 # hae 正则
 # pattern = r'''(?:"|')(((?:[a-zA-Z]{1,10}://|//)[^"'/]{1,}\.[a-zA-Z]{2,}[^"']{0,})|((?:/|\.\./|\./)[^"'><,;|*()(%%$^/\\\[\]][^"'><,;|()]{1,})|([a-zA-Z0-9_\-/]{1,}/[a-zA-Z0-9_\-/]{1,}\.(?:[a-zA-Z]{1,4}|action)(?:[\?|#][^"|']{0,}|))|([a-zA-Z0-9_\-/]{1,}/[a-zA-Z0-9_\-/]{3,}(?:[\?|#][^"|']{0,}|))|([a-zA-Z0-9_\-]{1,}\.(?:\w)(?:[\?|#][^"|']{0,}|)))(?:"|')'''
 
-#经过修改的 linkfinder 正则
+# linkfinder 正则
 pattern = r"""
 
   (?:"|')                               # Start newline delimiter
@@ -17,8 +17,8 @@ pattern = r"""
 
     |
 
-    ((?:/|\.\./|\./|\#)                    # Start with /,../,./,#
-    [^"'><,;| *()(%%$^\\\[\]]          # Next character can't be...
+    ((?:/|\.\./|\./|\#|[a-zA-Z0-9])     # Start with /,../,./, 数字, 字母
+    [^"'><,;| *()(%%$^\\\[\]]           # Next character can't be...
     [^"'><,;|()]{1,})                   # Rest of the characters can't be
 
     |
@@ -62,18 +62,17 @@ def find(response_text):
 
     for match in matches:
         path = match[0]
-        if ("http://" not in path and "https://" not in path) and not any(
-                i in match[0].lower() for i in black_list):
-
-            # if "./" in path:
-            #    path = path.replace("./", '\n'+'/')
-            for i in range(1, 6):
-                if path:
-                    if path.startswith('/'):
-                        path = path[1:]
-                    # elif path.startswith("#"):
-                    #    path = path[1:]
-                    elif path.startswith('.'):
-                        path = path[1:]
-            config.set_path(path.strip())
+        if '/' in path:
+            if ("http://" not in path and "https://" not in path) and not any(
+                    i in match[0].lower() for i in black_list):
+    
+                # if "./" in path:
+                #    path = path.replace("./", '\n'+'/')
+                for i in range(1, 6):
+                    if path:
+                        if path.startswith('/'):
+                            path = path[1:]
+                        elif path.startswith('.'):
+                            path = path[1:]
+                config.set_path(path.strip())
     return
